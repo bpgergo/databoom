e2fb97af865ed017eb8faa8170c0d662b2d82740
bpgergo <bpgergo@gmail.com>
2016-05-04 16:51:52 +0200

    an other demo frontend, with vizuly weighted tree

--- create.sql
+++ create.sql
@@ -37,3 +37,32 @@ amount 		numeric not null,
 comm 		text,
 tags 		text
 );
+
+
+create view func_tree as
+select
+fff.value  || ' (' || fff.id || ')' parent_parent_cofog_name,
+ff.value || ' (' || ff.id || ')' parent_cofog_name ,
+f.value || ' (' || f.id || ')' cofog_name ,
+b.amount
+from budget b
+inner join func f on (f.id = b.func_id)
+left outer join func ff on (ff.id = f.parent_id)
+left outer join func fff on (fff.id = ff.parent_id)
+where amount > 0
+;
+
+create view econ_tree as
+select
+eee.value  || ' (' || eee.id || ')' parent_parent_econ_name,
+ee.id ||' - '||ee.value  || ' (' || ee.id || ')' parent_econ_name,
+e.value || ' (' || e.id || ')' econ_name  ,
+b.amount
+from budget b
+inner join econ e on (e.id = b.econ_id)
+left outer join econ ee on (ee.id = e.parent_id)
+left outer join econ eee on (eee.id = ee.parent_id)
+where amount > 0;
+
+
+-- copy (select * from econ_tree) to '/tmp/postgres_export/econ_tree.csv' with csv
